7ca7cb742e25336757771d9c325fa50337c89871
nvinnikov <88853518+nvinnikov@users.noreply.github.com>
2023-06-15 03:44:50 +0300

    Update graph.cu

--- lab4/graph.cu
+++ lab4/graph.cu
@@ -1,62 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cuda_runtime.h>
 
-__global__ void breadthFirstSearch(int* graph, bool* visited, int numVertices, int startVertex) {
-    visited[startVertex] = true;
-    printf("%d ", startVertex);
+__global__ void breadthFirstSearch(const int* graph, bool* visited, int startNode, int numNodes) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    std::queue<int> vertexQueue;
-    vertexQueue.push(startVertex);
+    if (tid == startNode) {
+        visited[tid] = true;
+        printf("Visiting node %d\n", tid);
 
-    while (!vertexQueue.empty()) {
-        int currentVertex = vertexQueue.front();
-        vertexQueue.pop();
+        // Use a device-specific data structure or algorithm to implement BFS
 
-        for (int neighbor = 0; neighbor < numVertices; ++neighbor) {
-            if (graph[currentVertex * numVertices + neighbor] && !visited[neighbor]) {
-                visited[neighbor] = true;
-                printf("%d ", neighbor);
-                vertexQueue.push(neighbor);
+        // Example: Use atomic operations for synchronization
+        for (int i = 0; i < numNodes; ++i) {
+            if (graph[tid * numNodes + i] == 1 && !visited[i]) {
+                visited[i] = true;
+                printf("Visiting node %d\n", i);
             }
         }
     }
 }
 
 int main() {
-    int numVertices, numEdges;
-    std::cout << "Enter the number of vertices and edges: ";
-    std::cin >> numVertices >> numEdges;
+    int numNodes = 6;
+    std::vector<int> graph = {
+        0, 1, 1, 0, 0, 0,
+        1, 0, 0, 1, 0, 0,
+        1, 0, 0, 1, 1, 0,
+        0, 1, 1, 0, 1, 1,
+        0, 0, 1, 1, 0, 0,
+        0, 0, 0, 1, 0, 0
+    };
 
-    std::vector<int> graph(numVertices * numVertices, 0);
+    int startNode = 0;
 
-    std::cout << "Enter the edges:\n";
-    for (int i = 0; i < numEdges; ++i) {
-        int src, dest;
-        std::cin >> src >> dest;
+    int* deviceGraph;
+    bool* deviceVisited;
 
-        graph[src * numVertices + dest] = 1;
-        // Uncomment the line below for an undirected graph
-        // graph[dest * numVertices + src] = 1;
-    }
+    cudaMalloc((void**)&deviceGraph, numNodes * numNodes * sizeof(int));
+    cudaMemcpy(deviceGraph, graph.data(), numNodes * numNodes * sizeof(int), cudaMemcpyHostToDevice);
 
-    int startVertex;
-    std::cout << "Enter the starting vertex: ";
-    std::cin >> startVertex;
+    cudaMalloc((void**)&deviceVisited, numNodes * sizeof(bool));
+    cudaMemset(deviceVisited, false, numNodes * sizeof(bool));
 
-    int* d_graph;
-    bool* d_visited;
-    cudaMalloc((void**)&d_graph, numVertices * numVertices * sizeof(int));
-    cudaMalloc((void**)&d_visited, numVertices * sizeof(bool));
+    breadthFirstSearch<<<1, numNodes>>>(deviceGraph, deviceVisited, startNode, numNodes);
 
-    cudaMemcpy(d_graph, graph.data(), numVertices * numVertices * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemset(d_visited, 0, numVertices * sizeof(bool));
-
-    std::cout << "Breadth-first traversal: ";
-    breadthFirstSearch<<<1, 1>>>(d_graph, d_visited, numVertices, startVertex);
-
-    cudaFree(d_graph);
-    cudaFree(d_visited);
+    cudaFree(deviceGraph);
+    cudaFree(deviceVisited);
 
     return 0;
 }
