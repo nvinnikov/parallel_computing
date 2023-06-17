02e29831bf72f8b65b6de80cf51f9ddd73be9063
nvinnikov <88853518+nvinnikov@users.noreply.github.com>
2023-06-17 15:31:57 +0300

    Update graph.cu

--- lab4/graph.cu
+++ lab4/graph.cu
@@ -3,22 +3,25 @@
 #include <queue>
 #include <cuda_runtime.h>
 
-__global__ void breadthFirstSearch(const int* graph, bool* visited, int startNode, int numNodes) {
+__global__ void breadthFirstSearch(const int* graph, bool* visited, int startNode, int numNodes, double* executionTime) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (tid == startNode) {
         visited[tid] = true;
         printf("Visiting node %d\n", tid);
 
-        // Use a device-specific data structure or algorithm to implement BFS
 
-        // Example: Use atomic operations for synchronization
         for (int i = 0; i < numNodes; ++i) {
             if (graph[tid * numNodes + i] == 1 && !visited[i]) {
                 visited[i] = true;
                 printf("Visiting node %d\n", i);
             }
         }
+    }
+    __syncthreads();
+
+    if (tid == 0) {
+        *executionTime = clock() / static_cast<double>(CLOCKS_PER_SEC);
     }
 }
 
@@ -37,6 +40,7 @@ int main() {
 
     int* deviceGraph;
     bool* deviceVisited;
+    double* deviceExecutionTime;
 
     cudaMalloc((void**)&deviceGraph, numNodes * numNodes * sizeof(int));
     cudaMemcpy(deviceGraph, graph.data(), numNodes * numNodes * sizeof(int), cudaMemcpyHostToDevice);
@@ -44,10 +48,18 @@ int main() {
     cudaMalloc((void**)&deviceVisited, numNodes * sizeof(bool));
     cudaMemset(deviceVisited, false, numNodes * sizeof(bool));
 
-    breadthFirstSearch<<<1, numNodes>>>(deviceGraph, deviceVisited, startNode, numNodes);
+    cudaMalloc((void**)&deviceExecutionTime, sizeof(double));
+    cudaMemset(deviceExecutionTime, 0, sizeof(double));
+
+    breadthFirstSearch<<<1, numNodes>>>(deviceGraph, deviceVisited, startNode, numNodes, deviceExecutionTime);
+
+    double executionTime;
+    cudaMemcpy(&executionTime, deviceExecutionTime, sizeof(double), cudaMemcpyDeviceToHost);
+    std::cout << "Execution Time: " << executionTime << " seconds" << std::endl;
 
     cudaFree(deviceGraph);
     cudaFree(deviceVisited);
+    cudaFree(deviceExecutionTime);
 
     return 0;
 }
